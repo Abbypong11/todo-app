7898c8ad8bea5012fc84a42197bdc0279c320b58
Abigail Oppong <abbypong11@gmail.com>
2024-02-22 13:15:21 +0000

    working on AddTodo

--- src/components/add-todo/index.js
+++ src/components/add-todo/index.js
@@ -1,11 +1,42 @@
-import "./index.css";
+import styles from "./index.module.css";
+import { useLocalStorage } from "usehooks-ts";
+import { useState } from "react";
+
 function AddTodo() {
+    const [todos, setTodos] = useLocalStorage
+        ("TODO_KEY", []);
+    // let todo;
+    const [todo, setTodo] = useState("");
+
+    function collectInput(event) {
+        // todo = event.target.value;
+        setTodo(event.target.value);
+
+    }
+    //    const collectInput = (event) =>{
+    //     setTodo(event.target.value);
+
+    function saveTodo() {
+        //Add new todo to existing list of todos
+        //todos.push(todo);
+        //Set all todos in local storage
+        setTodos([...todos, todo]);
+    }
+
+
+
     return (
-       <section>
-        <input placeholder="start typing..."/>
-        <button> Create</button>
-       </section>
+        <section className={styles.addTodo}>
+            <input
+                onChange={collectInput}
+                // console.log(event.target.value);
+                // let todo = event.target.value;
+
+                className={styles.addTodoInput}
+                placeholder="start typing..." />
+            <button> Create</button>
+        </section>
     );
 }
 
-export default  AddTodo;
+export default AddTodo;
--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,10 +1,18 @@
+
+import { useLocalStorage } from "usehooks-ts";
+
 function Todolist() {
-    return (
+    const[todos, setTodos] = useLocalStorage
+        ("TODO_KEY", []); 
+
+        
+        return (
         <ul>
             <li>Complete online javascript course</li>
             <li>Jog around the park 3 times</li>
         </ul>
     );
-}
+        }
+
 
 export default Todolist
